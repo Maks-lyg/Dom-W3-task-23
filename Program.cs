869f03ac2bba4c5edb8d98219b6559c2d4559004
Maks <maks-lug333@yandex.ru>
2023-02-22 18:23:09 +0900

    ok

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine());
+int[] resalt = new int [x];
+for (int i = 1; i <= x; i++)
+    resalt [i - 1] = i * i * i;
+for (int i = 0; i <= x; i++)
+    Console.Write($"{resalt[i]} ");
